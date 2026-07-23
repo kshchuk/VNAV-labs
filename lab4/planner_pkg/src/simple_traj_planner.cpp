14949389ba94451942b0fde255df88175a21e801
Yarik Kishchuk <kshchuk@gmail.com>
2026-07-23 15:58:34 +0300

    Implement Lab 4 Part 0 simple planner to hold the first gate waypoint.
    Publish desired_state with zero velocity/acceleration and broadcast the target pose as TF for Gazebo control and visualization.

--- lab4/planner_pkg/src/simple_traj_planner.cpp
+++ lab4/planner_pkg/src/simple_traj_planner.cpp
@@ -56,7 +56,48 @@ private:
     //   4. Create and publish TF transform of the desired pose
     // ~~~~ begin solution
 
+    // 1. Extract the first vertex of the trajectory
+    const auto &first_pose = traj_msg->poses[0];
+
     geometry_msgs::msg::Transform transform;
+    transform.translation.x = first_pose.position.x;
+    transform.translation.y = first_pose.position.y;
+    transform.translation.z = first_pose.position.z;
+    transform.rotation = first_pose.orientation;
+
+    // 2. Set the acceleration and velocities to zero
+    geometry_msgs::msg::Twist velocity;
+    velocity.linear.x = 0.0;
+    velocity.linear.y = 0.0;
+    velocity.linear.z = 0.0;
+    velocity.angular.x = 0.0;
+    velocity.angular.y = 0.0;
+    velocity.angular.z = 0.0;
+
+    geometry_msgs::msg::Twist acceleration;
+    acceleration.linear.x = 0.0;
+    acceleration.linear.y = 0.0;
+    acceleration.linear.z = 0.0;
+    acceleration.angular.x = 0.0;
+    acceleration.angular.y = 0.0;
+    acceleration.angular.z = 0.0;
+
+    // 3. Publish the desired MultiDOFJointTrajectoryPoint
+    trajectory_msgs::msg::MultiDOFJointTrajectoryPoint desired_state;
+    desired_state.transforms.push_back(transform);
+    desired_state.velocities.push_back(velocity);
+    desired_state.accelerations.push_back(acceleration);
+
+    desired_state_pub_->publish(desired_state);
+
+    // 4. Create and publish TF transform of the desired pose
+    geometry_msgs::msg::TransformStamped tf_stamped;
+    tf_stamped.header.stamp = this->now();
+    tf_stamped.header.frame_id = traj_msg->header.frame_id.empty() ? "world" : traj_msg->header.frame_id;
+    tf_stamped.child_frame_id = "desired_pose";
+    tf_stamped.transform = transform;
+
+    br_->sendTransform(tf_stamped);
 
     // ~~~~ end solution
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
